e0dd6245b4d0ac44dca7415ef226fa1c33d1c60f
richardanixd-beep <richardanixd@gmail.com>
2025-11-27 06:08:11 +0800

    luxury hotel

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const User = require("models/User");
+const User = require("/models/User");
 
 // REGISTER
 const register = async (req, res) => {
